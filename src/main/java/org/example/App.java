86c55c8f10096abb7f9fdc5e0da811defc57e12d
nick fan <nickfan81@gmail.com>
2022-06-13 16:52:59 +0800

    feat: init project.

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+import cn.hutool.core.lang.Console;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//        System.out.println( "Hello World!" );
+        Console.log("Hello World!");
+
+    }
+}
